44228b71795ebf072f26683daad4c733c23f6730
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-06-02 12:43:49 +0530

    category model

--- chat/chat/settings/settings.py
+++ chat/chat/settings/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "account",
+    "server",
 ]
 
 MIDDLEWARE = [
@@ -113,3 +115,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "account.Account"
--- chat/server/models.py
+++ chat/server/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class Server(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE)
