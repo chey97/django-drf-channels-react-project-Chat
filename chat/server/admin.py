0415f2e1510ea8d678d3fc99732ee9118b5dcc9d
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-06-03 11:42:35 +0530

    Register models Channel,Category and Server

--- chat/server/admin.py
+++ chat/server/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Channel, Category, Server
+
+admin.site.register(Channel)
+admin.site.register(Category)
+admin.site.register(Server)
+ 
