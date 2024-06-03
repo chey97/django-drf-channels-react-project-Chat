e7e4b00a5bf726aee6584b9f9c6d6b40118bd819
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-06-03 16:13:18 +0530

    Filtering Servers by Server ID

--- chat/server/views.py
+++ chat/server/views.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import ValidationError
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Server
@@ -14,16 +15,27 @@ class ServerListViewSet(viewsets.ViewSet):
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user")
+        by_serverid = request.query_params.get("by_serverid")
 
         if category:
             queryset = queryset.filter(category__name=category)
-            
+
         if by_user:
             user_id = request.user.id
             queryset = queryset.filter(member=user_id)
 
         if qty:
             queryset = queryset[: int(qty)]
+
+        if by_serverid:
+            try:
+                queryset = queryset.filter(id=by_serverid)
+                if not queryset.exists():
+                    raise ValidationError(
+                        detail=f"Server with id {by_serverid} not found"
+                    )
+            except ValueError:
+                raise ValidationError(detail=f"Server id {by_serverid} is not valid")
 
         serializer = ServerSerializer(queryset, many=True)
         return Response(serializer.data)
