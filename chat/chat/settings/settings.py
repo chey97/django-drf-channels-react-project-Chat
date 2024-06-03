e87ceb7126a981f8bf6b061ded4b2b01a8218685
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-06-03 11:41:09 +0530

    New External Apps | Spectacular settings

--- chat/chat/settings/settings.py
+++ chat/chat/settings/settings.py
@@ -29,6 +29,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # External Apps
+    "drf_spectacular",
+    "rest_framework",
+    # Internal Apps
     "account",
     "server",
 ]
@@ -70,7 +74,7 @@ WSGI_APPLICATION = "chat.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / ".." / "db.sqlite3",
     }
 }
 
@@ -117,3 +121,17 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "account.Account"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': True,
+}
