a1bf65afb031b8ce1e8055818fe38907a83ea325
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-06-03 18:02:45 +0530

    create : server associated chnnels

--- chat/server/serializers.py
+++ chat/server/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
-from .models import Category, Server
+from .models import Category, Server, Channel
 
 
-class CategorySerializer(serializers.ModelSerializer):
+class ChannelSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Category
+        model = Channel
         fields = "__all__"
 
 
 class ServerSerializer(serializers.ModelSerializer):
+    channel_server = ChannelSerializer(many=True)
+    
     class Meta:
         model = Server
         fields = "__all__"
