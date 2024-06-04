5e196c64f151d515eb154cb19db5868b2e69fa79
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-06-04 16:52:44 +0530

    with_num_members : count number of memebers in a server

--- chat/server/serializers.py
+++ chat/server/serializers.py
@@ -9,8 +9,17 @@ class ChannelSerializer(serializers.ModelSerializer):
 
 
 class ServerSerializer(serializers.ModelSerializer):
+    num_members = serializers.SerializerMethodField()
     channel_server = ChannelSerializer(many=True)
     
     class Meta:
         model = Server
-        fields = "__all__"
+        exclude = ("member",)
+
+    def get_num_members(self, obj):
+        if hasattr(obj, "num_members"):
+            return obj.num_members
+        return None
+    
+    def to_representation(self, instance):
+        return super().to_representation(instance)
--- chat/server/views.py
+++ chat/server/views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets
 from .models import Server
 from .serializers import ServerSerializer
 from rest_framework.response import Response
+from django.db.models import Count
 
 
 class ServerListViewSet(viewsets.ViewSet):
@@ -14,8 +15,9 @@ class ServerListViewSet(viewsets.ViewSet):
 
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
-        by_user = request.query_params.get("by_user")
+        by_user = request.query_params.get("by_user") == "true"
         by_serverid = request.query_params.get("by_serverid")
+        with_num_members = request.query_params.get("with_num_members") == "true"
 
         if (by_user or by_serverid) and not request.user.is_authenticated:
             raise AuthenticationFailed()
@@ -26,6 +28,9 @@ class ServerListViewSet(viewsets.ViewSet):
         if by_user:
             user_id = request.user.id
             queryset = queryset.filter(member=user_id)
+            
+        if with_num_members:
+            queryset = queryset.annotate(num_members=Count("member"))
 
         if qty:
             queryset = queryset[: int(qty)]
@@ -40,5 +45,5 @@ class ServerListViewSet(viewsets.ViewSet):
             except ValueError:
                 raise ValidationError(detail=f"Server id {by_serverid} is not valid")
 
-        serializer = ServerSerializer(queryset, many=True)
+        serializer = ServerSerializer(queryset, many=True, context={"num_members": with_num_members})
         return Response(serializer.data)
