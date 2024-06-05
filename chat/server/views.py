3a3db34381bea749590040896a39016a30405b7f
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-06-05 22:25:58 +0530

    Added Docstrings

--- chat/server/views.py
+++ chat/server/views.py
@@ -5,12 +5,32 @@ from .models import Server
 from .serializers import ServerSerializer
 from rest_framework.response import Response
 from django.db.models import Count
+from .schema import server_list_docs
 
 
 class ServerListViewSet(viewsets.ViewSet):
     queryset = Server.objects.all()
 
+    @server_list_docs
     def list(self, request):
+        """
+        Handles GET requests to list Server objects with various filtering options.
+
+        Query Parameters:
+        - category (str, optional): Filter servers by category name.
+        - qty (int, optional): Limit the number of results returned.
+        - by_user (str, optional): If "true", filter servers where the current user is a member.
+        - by_serverid (str, optional): Filter by specific server ID.
+        - with_num_members (str, optional): If "true", include the number of members in each server.
+
+        Raises:
+        - AuthenticationFailed: If the user is not authenticated and requests filtering by user or server ID.
+        - ValidationError: If the server ID is not valid or no server with the given ID is found.
+
+        Returns:
+        - Response: A JSON response containing the serialized server data.
+        """
+
         queryset = Server.objects.all()
 
         category = request.query_params.get("category")
@@ -19,23 +39,26 @@ class ServerListViewSet(viewsets.ViewSet):
         by_serverid = request.query_params.get("by_serverid")
         with_num_members = request.query_params.get("with_num_members") == "true"
 
-        if (by_user or by_serverid) and not request.user.is_authenticated:
-            raise AuthenticationFailed()
+        # if (by_user or by_serverid) and not request.user.is_authenticated:
+        #     raise AuthenticationFailed()
 
         if category:
             queryset = queryset.filter(category__name=category)
 
         if by_user:
-            user_id = request.user.id
-            queryset = queryset.filter(member=user_id)
-            
+            if by_user and request.user.is_authenticated:
+                user_id = request.user.id
+                queryset = queryset.filter(member=user_id)
+            else:
+                raise AuthenticationFailed()
+
         if with_num_members:
             queryset = queryset.annotate(num_members=Count("member"))
 
-        if qty:
-            queryset = queryset[: int(qty)]
-
         if by_serverid:
+            if not request.user.is_authenticated:
+                raise AuthenticationFailed()
+            
             try:
                 queryset = queryset.filter(id=by_serverid)
                 if not queryset.exists():
@@ -44,6 +67,11 @@ class ServerListViewSet(viewsets.ViewSet):
                     )
             except ValueError:
                 raise ValidationError(detail=f"Server id {by_serverid} is not valid")
+            
+        if qty:
+            queryset = queryset[: int(qty)]
 
-        serializer = ServerSerializer(queryset, many=True, context={"num_members": with_num_members})
+        serializer = ServerSerializer(
+            queryset, many=True, context={"num_members": with_num_members}
+        )
         return Response(serializer.data)
