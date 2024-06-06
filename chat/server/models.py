f49ff7c30717f0bd499b04dfb508b69efe85a799
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-06-06 09:09:55 +0530

    Bug fix: take uuid in category model

--- chat/server/models.py
+++ chat/server/models.py
@@ -1,23 +1,36 @@
+import uuid
 from django.db import models
 from django.conf import settings
 from django.shortcuts import get_object_or_404
+from django.dispatch import receiver
+from django.db.models.signals import pre_delete
+
 
 # Create your models here.
 
+
 def category_icon_upload_path(instance, filename):
-    return f"category/{instance.id}/category_icon/{filename}"
+    return f"category/{instance.uuid}/category_icon/{filename}"
+
 
 class Category(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     icon = models.FileField(upload_to=category_icon_upload_path, blank=True, null=True)
-    
-    def save(self, *args, **kwargs):
-        if self.id:
-            existing = get_object_or_404(Category, id=self.id)
-            if existing.icon != self.icon:
-                existing.icon.delete(save=False)
-                super(Category, self).save(*args, **kwargs)
+
+    # def save(self, *args, **kwargs):
+    #     if self.id:
+    #         existing = get_object_or_404(Category, id=self.id)
+    #         if existing.icon != self.icon:
+    #             existing.icon.delete(save=False)
+    #             super(Category, self).save(*args, **kwargs)
+
+    # @receiver(models.signals.pre_delete, sender="server.Category")
+    # def category_delete_files(sender, instance, **kwargs):
+    #     for field in instance._meta.fields:
+    #         if field.name == "icon":
+    #             instance.icon.delete(save=False)
 
     def __str__(self):
         return self.name
@@ -25,8 +38,12 @@ class Category(models.Model):
 
 class Server(models.Model):
     name = models.CharField(max_length=100)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="server_owner")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="server_category")
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="server_owner"
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="server_category"
+    )
     description = models.CharField(max_length=250, blank=True, null=True)
     member = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
@@ -36,9 +53,13 @@ class Server(models.Model):
 
 class Channel(models.Model):
     name = models.CharField(max_length=100)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner")
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner"
+    )
     topic = models.CharField(max_length=250)
-    server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name="channel_server")
+    server = models.ForeignKey(
+        Server, on_delete=models.CASCADE, related_name="channel_server"
+    )
 
     def save(self, *args, **kwargs):
         self.name = self.name.lower()
