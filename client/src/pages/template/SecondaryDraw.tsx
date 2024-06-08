9f32a0ce446b3483c47fda2165938c16d571fd2a
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-06-08 22:59:38 +0530

    Secondary drawer updated

--- client/src/pages/template/SecondaryDraw.tsx
+++ client/src/pages/template/SecondaryDraw.tsx
@@ -1,46 +1,25 @@
-import {
-  Box,
-  Drawer,
-  Typography,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
-import { useEffect, useState } from "react";
+import { Box, Typography, useTheme } from "@mui/material";
 
 const SecondaryDraw = () => {
   const theme = useTheme();
-  const below600 = useMediaQuery("(max-width:599px)");
-  const [open, setOpen] = useState(true);
-
-  useEffect(() => {
-    setOpen(!below600);
-  }, [below600]);
-
-  if (below600) return null;
-
   return (
-    <Drawer
-      open={open}
-      variant={"permanent"}
-      PaperProps={{
-        sx: {
-          mt: `${theme.primaryAppBar.height}px`,
-          ml: `${theme.primaryDraw.width}px`,
-          height: `calc(100vh - ${theme.primaryAppBar.height})px`,
-          width: theme.secondaryDraw.width,
-        },
+    <Box
+      sx={{
+        minWidth: `${theme.secondaryDraw.width}px`,
+        height: `calc(100vh - ${theme.primaryAppBar.height}px)`,
+        mt: `${theme.primaryAppBar.height}px`,
+        borderRight: `1px solid ${theme.palette.divider}`,
+        display: { xs: "none", sm: "block" },
+        overflowY: "auto",
+        overflowX: "hidden",
       }}
     >
-      <Box>
-        <Box>
-          {[...Array(100)].map((_, i) => (
-            <Typography key={i} paragraph>
-              {i + 1}
-            </Typography>
-          ))}
-        </Box>
-      </Box>
-    </Drawer>
+      {[...Array(100)].map((_, i) => (
+        <Typography key={i} paragraph>
+          {i + 1}
+        </Typography>
+      ))}
+    </Box>
   );
 };
 export default SecondaryDraw;
