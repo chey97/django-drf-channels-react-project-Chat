5b0a7f87566d9dcec73db9c2d0efcf3481add293
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-06-08 23:43:02 +0530

    update : axios get request for a cat type

--- client/src/pages/template/SecondaryDraw.tsx
+++ client/src/pages/template/SecondaryDraw.tsx
@@ -1,7 +1,18 @@
 import { Box, Typography, useTheme } from "@mui/material";
+import axios from "axios";
 
 const SecondaryDraw = () => {
   const theme = useTheme();
+
+  axios
+    .get("http://127.0.0.1:8000/api/server/select/?category=cat1")
+    .then((respose) => {
+      console.log(respose.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
   return (
     <Box
       sx={{
