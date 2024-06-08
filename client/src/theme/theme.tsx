0ee86dc2171b460cef46f13b398d5419255969f2
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-06-08 19:22:05 +0530

    added themes for drawers

--- client/src/theme/theme.tsx
+++ client/src/theme/theme.tsx
@@ -5,10 +5,24 @@ declare module "@mui/material/styles" {
     primaryAppBar: {
       height: number;
     };
+    primaryDraw: {
+      width: number;
+      closed: number;
+    };
+    secondaryDraw: {
+      width: number;
+    };
   }
   interface ThemeOptions {
     primaryAppBar?: {
       height: number;
+    };
+    primaryDraw?: {
+      width: number;
+      closed: number;
+    };
+    secondaryDraw?: {
+      width: number;
     };
   }
 }
@@ -21,14 +35,21 @@ export const createMuiTheme = () => {
     primaryAppBar: {
       height: 50,
     },
+    primaryDraw: {
+      width: 240,
+      closed: 70,
+    },
+    secondaryDraw: {
+      width: 240,
+    },
     components: {
-        MuiAppBar: {
-            defaultProps: {
-                color: "default",
-                elevation: 0,
-            },
+      MuiAppBar: {
+        defaultProps: {
+          color: "default",
+          elevation: 0,
         },
-    }
+      },
+    },
   });
   theme = responsiveFontSizes(theme);
   return theme;
