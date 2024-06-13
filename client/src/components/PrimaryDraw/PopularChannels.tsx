55da0d7db029fa578f74cb9bf13f8e2fcd670ddc
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-06-13 22:17:56 +0530

    popularchannels : Pirmary drawer

--- client/src/components/PrimaryDraw/PopularChannels.tsx
+++ client/src/components/PrimaryDraw/PopularChannels.tsx
@@ -1,10 +1,44 @@
-import { Box, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Link,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { useEffect } from "react";
+import useCrud from "../../hooks/useCrud";
+import { MEDIA_URL } from "../../config";
+
+interface Server {
+  id: number;
+  name: string;
+  category: string;
+  icon: string;
+}
 
 type Props = {
   open: boolean;
 };
 
 const PopularChannels: React.FC<Props> = ({ open }) => {
+  const { dataCRUD, error, isLoading, fetchData } = useCrud<Server>(
+    [],
+    "/server/select/"
+  );
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // useEffect(() => {
+  //   console.log(dataCRUD);
+  // }, []);
+
   return (
     <>
       <Box
@@ -20,6 +54,68 @@ const PopularChannels: React.FC<Props> = ({ open }) => {
           Popular
         </Typography>
       </Box>
+      <List>
+        {dataCRUD.map((item) => (
+          <ListItem
+            key={item.id}
+            disablePadding
+            sx={{ display: "block" }}
+            dense={true}
+          >
+            <Link
+              to={`/server/${item.id}`}
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <ListItemButton sx={{ minHeight: 0 }}>
+                <ListItemIcon sx={{ minWidth: 0, justifyContent: "center" }}>
+                  <ListItemAvatar sx={{ minWidth: "50px" }}>
+                    <Avatar
+                      alt="Server Icon"
+                      src={`${MEDIA_URL}${item.icon}`}
+                    />
+                  </ListItemAvatar>
+                </ListItemIcon>
+                <ListItemText
+                  primary={
+                    <Typography
+                      variant="body2"
+                      sx={{
+                        fontWeight: 700,
+                        lineHeight: 1.2,
+                        textOverFlow: "ellipsis",
+                        overflow: "hidden",
+                        whiteSpace: "nowrap",
+                      }}
+                    >
+                      {item.name}
+                    </Typography>
+                  }
+                  secondary={
+                    <Typography
+                      variant="body2"
+                      sx={{
+                        fontWeight: 500,
+                        lineHeight: 1.2,
+                        color: "textSecondary",
+                      }}
+                    >
+                      {item.category}
+                    </Typography>
+                  }
+                  sx={{ opacity: open ? 1 : 0 }}
+                  primaryTypographyProps={{
+                    sx: {
+                      textOverFlow: "ellipsis",
+                      overflow: "hidden",
+                      whiteSpace: "nowrap",
+                    },
+                  }}
+                />
+              </ListItemButton>
+            </Link>
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 };
