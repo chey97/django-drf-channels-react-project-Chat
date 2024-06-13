ed436f3ec658edee9b5e7326b2493a93596a7c60
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-06-13 15:51:58 +0530

    Custom Hook: useCRUD

--- client/src/hooks/useCrud.ts
+++ client/src/hooks/useCrud.ts
@@ -1,14 +1,39 @@
 import useAxiosWIthInterceptor from "../helpers/jwtinterceptor";
 import { BASE_URL } from "../config";
+import { useState } from "react";
 
-const useCrud = () => {
+interface IuseCrud<T> {
+    dataCRUD: T[];
+    fetchData: () => Promise<void>;
+    error: Error | null;
+    isLoading: boolean;
+}
+
+const useCrud = <T>(initialData: T[], apiURL: string): IuseCrud<T> => {
     const jwtAxios = useAxiosWIthInterceptor();
+    const [dataCRUD, setDataCRUD] = useState<T[]>(initialData)
+    const [error, setError] = useState<Error | null>(null)
+    const [isLoading, setIsLoading] = useState(false)
 
-    const fetchData = () => {
+    const fetchData = async () => {
+        setIsLoading(true)
+        try {
+            const response = await jwtAxios.get(`${BASE_URL}${apiURL}`, {})
+            const data = response.data
+            setDataCRUD(data)
+            setError(null)
+            setIsLoading(false)
+            return data;
+        } catch (error: any) {
+            if (error.response && error.response.status === 400) {
+                setError(new Error("400"))
+            }
+            setIsLoading(false)
+                throw error;
+        }
         // logic
-    }
-    
-    return{fetchData}
+    };
+    return { fetchData, dataCRUD, error, isLoading}
 }
 
 export default useCrud;
